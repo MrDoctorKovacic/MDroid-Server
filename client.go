7cd3d3afeea2b7bdf5b9bc57e6a8529292bf92dd
Quinn Casey <qcasey1@gmail.com>
2019-09-25 20:24:43 -0700

    increasing write buffer size

--- client.go
+++ client.go
@@ -31,7 +31,7 @@ var (
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  4096,
-	WriteBufferSize: 1024,
+	WriteBufferSize: 4096,
 }
 
 // Client is a middleman between the websocket connection and the hub.
